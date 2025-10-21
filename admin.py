39b947a30ec55d39053ca8a33d010c5dfa24694b
Chris Karr <github@audacious-software.com>
2025-10-21 12:18:12 -0500

    Update admin.py

--- admin.py
+++ admin.py
@@ -2,22 +2,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import django
+
 from django.contrib.gis import admin
+
+if django.get_version() < '5':
+    from django.contrib.gis.admin import OSMGeoAdmin as GISModelAdmin # pylint: disable=no-name-in-module
+else:
+    from django.contrib.gis.admin import GISModelAdmin # pylint: disable=no-name-in-module
 
 from .models import ExternalDataSource, ExternalDataRequest, ExternalDataRequestFile
 
 @admin.register(ExternalDataSource)
-class ExternalDataSourceAdmin(admin.OSMGeoAdmin):
+class ExternalDataSourceAdmin(GISModelAdmin):
     list_display = ('name', 'identifier', 'priority',)
     list_filter = ('name', 'identifier',)
 
 @admin.register(ExternalDataRequest)
-class ExternalDataRequestAdmin(admin.OSMGeoAdmin):
+class ExternalDataRequestAdmin(GISModelAdmin):
     list_display = ('pk', 'requested', 'identifier', 'email', 'can_email', 'last_emailed', 'completed',)
     list_filter = ('requested', 'sources', 'can_email',)
     search_fields = ('identifier', 'token', 'extras',)
 
 @admin.register(ExternalDataRequestFile)
-class ExternalDataRequestFileAdmin(admin.OSMGeoAdmin):
+class ExternalDataRequestFileAdmin(GISModelAdmin):
     list_display = ('request', 'id', 'source', 'uploaded', 'processed', 'skipped', 'encrypted',)
     list_filter = ('processed', 'skipped', 'uploaded', 'source', 'request')
