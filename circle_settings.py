07eed37ed65a339d42e906d0d7ed06ce9dd50f0c
Chris Karr <github@audacious-software.com>
2026-01-20 10:05:41 -0600

    Update circle_settings.py

--- circle_settings.py
+++ circle_settings.py
@@ -67,7 +67,7 @@ DATABASES = {
         'ENGINE':   'django.contrib.gis.db.backends.postgis',
         'NAME':     'circle_test',
         'USER':     'root',
-        'PASSWORD': '',
+        'PASSWORD': '', # nosec
         'HOST':     'localhost',
         'PORT':     '',
     }
